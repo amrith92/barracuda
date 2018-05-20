9a96e5e05d3926d49bedffa7ec43dbb608fcf366
Amrith Nayak <amrith92@gmail.com>
2018-05-20 13:49:02 +0530

    - Mixed some react in

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,1 +1,17 @@
-console.log(`'Allo 'Allo! Popup`)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+
+  render() {
+    return (
+      <div><h3>Yo!</h3></div>
+    )
+  }
+}
+
+ReactDOM.render(<Home/>, document.getElementById('app'));
